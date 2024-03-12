6691e0796e68e54a2e0c6040c15c15c247bc5b41
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2024-03-12 15:38:09 +0530

    setup react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Vite App</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div>
+        <h1>Heading</h1>
+        <p>Paragraph</p>
+    </div>
+  )
+}
+
+export default Card
